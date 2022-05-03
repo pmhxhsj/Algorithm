c88f32ac82f3ff3eabed2e7d048672abf72f2340
pmhxhsj <minho7952@naver.com>
2022-05-04 00:21:42 +0900

    baekjoon 2108

--- baekjoon/구현/2108.js
+++ baekjoon/구현/2108.js
@@ -1,38 +1,47 @@
-const fs = require('fs');
-let input = fs.readFileSync('../input.txt').toString().trim().split('\n');
-input.shift();
-input = input.map((v) => +v).sort((a, b) => a - b);
+const readline = require('readline');
 
-// 평균
-console.log(Math.round(input.reduce((prev, cur) => prev + cur, 0) / input.length));
-// 중앙값
-console.log(input[(input.length - 1) / 2]);
-
-let obj = {};
-
-for (let i = 0; i < input.length; i++) {
-  if (obj.hasOwnProperty(String(input[i]))) {
-    obj[String(input[i])] += 1;
-  } else {
-    obj[String(input[i])] = 1;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input = [];
+rl.on('line', function (line) {
+  input.push(line);
+  if (input.length === +input[0] + 1) {
+    rl.close();
   }
-}
+}).on('close', function () {
+  const [N, ...nums] = input.map(Number);
 
-obj = Object.entries(obj).sort(([a, b], [c, d]) => {
-  if (b === d) {
-    return a - c;
+  nums.sort((a, b) => a - b);
+
+  const avg = Math.round(nums.reduce((acc, cur) => acc + cur, 0) / nums.length);
+  const median = nums[Math.floor(nums.length / 2)];
+
+  const obj = {};
+
+  for (let i = 0; i < N; i++) {
+    if (!obj[nums[i]]) {
+      obj[nums[i]] = 1;
+    } else {
+      obj[nums[i]] += 1;
+    }
+  }
+  const modeArr = Object.entries(obj).sort((a, b) => b[1] - a[1]);
+  const value = modeArr[0][1];
+
+  console.log(avg === 0 ? +0 : avg);
+  console.log(median);
+  if (nums.length > 1 && modeArr[1][1] === value) {
+    const maxModeArr = modeArr.filter((v) => v[1] === value);
+    maxModeArr.sort((a, b) => +a[0] - +b[0]);
+    console.log(+maxModeArr[1][0]);
   } else {
-    return d - b;
+    console.log(+modeArr[0][0]);
+  }
+  if (nums.length > 1) {
+    console.log(Math.abs(nums[nums.length - 1] - nums[0]));
+  } else {
+    console.log(0);
   }
 });
-if (obj.length === 1) {
-  console.log(obj[0][0]);
-} else if (obj[0][1] === obj[1][1]) {
-  console.log(obj[1][0]);
-} else {
-  console.log(obj[0][0]);
-}
-// 최빈값
-
-// 범위
-console.log(Math.abs(Math.max(...input) - Math.min(...input)));
