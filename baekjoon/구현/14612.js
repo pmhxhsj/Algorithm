13fe52765eb659a549a202cceb35187364d5174f
pmhxhsj <minho7952@naver.com>
2022-05-05 01:57:51 +0900

    baekjoon 14612

--- baekjoon/구현/14612.js
+++ baekjoon/구현/14612.js
@@ -1,42 +1,51 @@
-const fs = require('fs');
-let input = fs.readFileSync('../input.txt').toString().trim().split('\n');
-input.shift();
+const readline = require('readline');
 
-let tableArr = [];
-let timeArr = [];
-const answer = [];
-
-input = input.map((v) => v.split(' '));
-
-console.log(input);
-
-input.forEach((v) => {
-  if (v[0] === 'order') {
-    const [table, time] = [v[1], v[2]];
-    tableArr.push(table);
-    answer.push(tableArr.join(' '));
-    timeArr.push([table, time]);
-  } else if (v[0] === 'sort') {
-    tableArr = timeArr
-      .sort((a, b) => {
-        if (a[1] === b[1]) {
-          return a[0] - b[0];
-        }
-        return a[1] - b[1];
-      })
-      .map((v1) => v1[0]);
-
-    answer.push(tableArr.join(' '));
-  } else {
-    const idx = tableArr.indexOf(v[1]);
-    tableArr.splice(idx, 1);
-    answer.push(tableArr.join(' '));
-  }
-  if (tableArr.length === 0) {
-    answer.push('sleep');
-    tableArr = [];
-    timeArr = [];
-  }
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+let input = [];
+rl.on('line', function (line) {
+  input.push(line);
+  if (input.length === +input[0].split(' ')[0] + 1) {
+    rl.close();
+  }
+}).on('close', function () {
+  const [N, M] = input.shift().split(' ').map(Number);
+  const commands = input.map((v) => v.split(' '));
+  const answer = [];
+  const result = [];
+  commands.forEach((ele) => {
+    if (ele[0] === 'order') {
+      result.push([ele[1], ele[2]].map(Number));
+      answer.push(result.map((v) => v[0]));
+    } else if (ele[0] === 'sort') {
+      if (result.length === 0) {
+        answer.push('sleep');
+      } else {
+        result.sort((a, b) => {
+          if (a[1] === b[1]) {
+            return a[0] - b[0];
+          } else {
+            return a[1] - b[1];
+          }
+        });
+        answer.push(result.map((v) => v[0]));
+      }
+    } else {
+      const resultFilterArr = result.map((v) => v[0]);
 
-console.log(answer.filter((v) => v.length !== 0).join('\n'));
+      if (resultFilterArr.includes(+ele[1])) {
+        const idx = resultFilterArr.indexOf(+ele[1]);
+        result.splice(idx, 1);
+      }
+
+      if (result.length > 0) {
+        answer.push(result.map((v) => v[0]));
+      } else {
+        answer.push('sleep');
+      }
+    }
+  });
+  console.log(answer.map((v) => (Array.isArray(v) ? v.join(' ') : v)).join('\n'));
+});
